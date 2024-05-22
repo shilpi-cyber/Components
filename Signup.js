5e546dd9f564e76175ffd291fda57b7cac3a88a0
Shilpi Kaushik <67389328+shilpi-cyber@users.noreply.github.com>
2024-05-23 00:16:18 +0530

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Signup = () => {
+  return (
+    <>
+    <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+  
+  <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+    
+    <div className="md:w-1/2 px-8 md:px-16">
+      <h2 className="font-bold text-2xl text-[#002D74]">Create Your Account</h2>
+      
+
+      <form action="" className="flex flex-col gap-4">
+        <input className="p-2 mt-8 rounded-xl border" type="text" name="firstname" placeholder="Name"/>
+        <div className="relative">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+  <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
+  <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+</svg>
+          <input className="p-2 rounded-xl border w-full" type="email" name="email" placeholder="Email"/>
+          
+        </div>
+        <div className="relative">
+        <input className="p-2 rounded-xl border w-full" type="tel" name="tel" placeholder="Phone number"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg>
+        </div>
+
+        <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">Sign up</button>
+      </form>
+
+      <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+        <hr className="border-gray-400"/>
+        <p className="text-center text-sm">OR</p>
+        <hr className="border-gray-400"/>
+      </div>
+
+      <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
+        <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+          <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
+          <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
+          <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
+          <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
+        </svg>
+        Signup with Google
+      </button>
+
+      <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+        <Link to="/" type='button'>Forgot your password?</Link>
+      </div>
+
+      <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+        <p>Already a member?</p>
+        <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Login</button>
+      </div>
+    </div>
+
+    <div className="md:block hidden w-1/2">
+      <img className="rounded-2xl" src="https://img.freepik.com/free-photo/view-electronic-product-balancing-podium_23-2150141321.jpg?t=st=1716401631~exp=1716405231~hmac=f767f9b5dadce1c30111344fd338d4e8aa2eaf29a2a9e0ef38e87a4287b65a78&w=360"/>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Signup
